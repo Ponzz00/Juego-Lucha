570e2bc52066f823490210b16a7f82015cc24426
Ponzz00 <57675878+Ponzz00@users.noreply.github.com>
2020-02-25 13:25:05 +0100

    Add files via upload

--- Juego/src/Lucha/main.java
+++ Juego/src/Lucha/main.java
@@ -14,7 +14,7 @@ public class main {
 					+ "// KAIO-ZEN=[30] // GENKIDAMA=[90]",50,30,150),
 			new Personajes( "Vegetta",300,100,"BIG-BANG ATTACK=[50] "
 					+ "// CANYON GALICK=[30] // FINAL FLASH=[90]",80,50,90),
-			new Personajes( "Freezer",160,180,"RAYO MORTAL=[50] "
+			new Personajes( "Gohan",160,180,"RAYO MORTAL=[50] "
 					+ "// CANYON COSMICO=[30] // SUPERNOVA=[90]",50,30,110) };
 		for(int i=0;i<Personaje.length;i++)//USAMOS (FOR) PARA MOSTRAR LA LISTA UNO POR UNO
 			 System.out.println(Personaje[i]); 
@@ -26,7 +26,7 @@ public class main {
 		System.out.println(
 					"\n0.GOKU"
 				+ "\n1.VEGETTA"
-				+ "\n2.FREEZER");
+				+ "\n2.GOHAN");
 		Scanner sc = new Scanner(System.in);
 		int J1;
 		int x = 0;//LO CREAMOS PARA QUE ME ESCOJA JUSTO EL VALOR QUE HE METIDO EN EL SWITCH
@@ -37,15 +37,17 @@ public class main {
 		case 0:
 			
 			System.out.println("Jugador 1 ha seleccionado a "+Personaje[0].getNombre());
-			
+			Personaje[0].Dibujo0();
 			break;
 			
 		case 1:
 			System.out.println("Jugador 1 ha seleccionado a "+Personaje[1].getNombre());
+			Personaje[0].Dibujo1();
 			break;
 			
 		case 2:
 			System.out.println("Jugador 1 ha seleccionado a "+Personaje[2].getNombre());
+			Personaje[0].Dibujo2();
 			break;
 		
 		
@@ -61,28 +63,31 @@ public class main {
 			case 0:
 				
 				System.out.println("Jugador 2 ha seleccionado a "+Personaje[0].getNombre());
-				
+				Personaje[0].Dibujo0();
 				break;
 				
 			case 1:
 				System.out.println("Jugador 2 ha seleccionado a "+Personaje[1].getNombre());
+				Personaje[0].Dibujo1();
 				break;
 				
 			case 2:
 				System.out.println("Jugador 2 ha seleccionado a "+Personaje[2].getNombre());
+				Personaje[0].Dibujo2();
 				break;
 			}
 			
 			System.out.println("\n-------------¡¡¡LUCHAD!!!-----------------");
 			
-			while(Personaje[J1].getVida()>0||Personaje[J2].getVida()>0) {
-			
+			while(Personaje[J1].getVida()>0||Personaje[J2].getVida()>0) { //CREE ESTE BUCLE PARA QUE SE FUESE REPITIENDO
+																	 //SECUENCIALMENTE HASTA QUE UNO MURIESE, PERO AL REINICIAR EL BUCLE, 
+																	//TAMBIEN REINICIA LA VIDA ASI QUE LO HARE DE LA MANERA LARGA.
 			System.out.println("\nEs el turno del Jugador 1"
 					+ "\n"
 					+ "\nSelecciona una habilidad: [0 / 1 / 2]"
 					+ "\n"
 					+"\n[ " + Personaje[J1].getT()+" ]");
-			
+			System.out.println("\n-----------------------------------------\n");
 
 			int stec=x;
 			stec=sc.nextInt();
@@ -116,12 +121,13 @@ public class main {
 			
 			
 			}
+			System.out.println("\n-----------------------------------------\n");
 			System.out.println("\nEs el turno del Jugador 2"
 					+ "\n"
 					+ "\nSelecciona una habilidad: [0 / 1 / 2]"
 					+ "\n"
 					+"\n[ " + Personaje[J2].getT()+" ]");
-				
+			System.out.println("\n-----------------------------------------\n");	
 			
 			int stec1=y;
 			stec1=sc.nextInt();
@@ -154,15 +160,13 @@ public class main {
 				restaStamina=Personaje[J2].getStamina()-50;
 				System.out.println("Stamina del Jugador 1 ha bajado a: "+restaStamina);
 				
-				break;
-			}
-			System.out.println("EL GANADOR ES...: ");
-			}
-			
+				break;}
+		
 	}
-	
-
-}			
+		
+				System.out.println("EL GANADOR ES...: ");
+		}
+}
 			
 		
 
